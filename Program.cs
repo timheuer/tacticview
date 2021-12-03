9b10641ff0da90dec9c89796ccaebd2fbf9c1dca
Tim Heuer <tim@timheuer.com>
2021-12-03 10:43:39 -0800

    updating dependencies

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Radzen;
 using TacticView.Data;
